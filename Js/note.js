ebe926e87b212c1159155c9039a19806e707f64d
evanleonille <evanleonille@gmail.com>
2023-10-21 10:04:54 +0800

    update

---
+++ Js/note.js
@@ -0,0 +1,4 @@
+const notesLink = document.getElementById("notesLink");
+        notesLink.addEventListener("click", function() {
+            $('#modalForm').modal('show'); // This line triggers the modal to appear
+        });
