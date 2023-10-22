69ce27890beccd9fda36a6813da5f1376d420019
evanleonille <evanleonille@gmail.com>
2023-10-22 23:51:03 +0800

    update

--- Js/note.js
+++ Js/note.js
@@ -1,4 +1,20 @@
-const notesLink = document.getElementById("notesLink");
-        notesLink.addEventListener("click", function() {
-            $('#modalForm').modal('show'); // This line triggers the modal to appear
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const threeDotMenu = document.querySelector(".three-dot-menu");
+    const dropdown = document.getElementById("myDropdown");
+    const notesLink = document.getElementById("notesLink");
+    const modalForm = document.getElementById("modalForm");
+    const closeForm = document.getElementById("closeForm");
+
+    threeDotMenu.addEventListener("click", function() {
+        dropdown.style.display = "block";
+    });
+
+    notesLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        modalForm.style.display = "block";
+    });
+
+    closeForm.addEventListener("click", function() {
+        modalForm.style.display = "none";
+    });
+});
